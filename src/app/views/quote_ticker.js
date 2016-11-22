24a87ff759bd44028c2c01b2cc15d9273089888a
Charles Ellis <hamled@hamled.name>
2016-11-22 06:19:51 -0800

    Quote ticker uses template for view contents
    In this change we create a template script inside of index.html which
    contains the HTML for a single quote ticker, and uses underscore
    template tags (<%= %>) to interpolate in some variables.
    
    Functionally this is a lot like how we use ERB in Rails.

--- src/app/views/quote_ticker.js
+++ src/app/views/quote_ticker.js
@@ -1,6 +1,14 @@
+import _ from 'underscore';
 import Backbone from 'backbone';
 
 const QuoteTicker = Backbone.View.extend({
+  initialize: function() {
+    // Setup our compiled template function using the template code
+    // from our index HTML
+
+    const templateString = Backbone.$('#tmpl-quote-ticker').html();
+    this.template = _.template(templateString);
+  }
 });
 
 export default QuoteTicker;
